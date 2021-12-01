cded2a7cb8af7cfe3bfa7d1c1da3dcd1306ed95c
sanjayqburst <sanjay.mohan@qbusrt.com>
2021-12-01 17:35:16 +0530

    Error on main activity line 14

--- app/src/main/java/com/example/apicalls/MainActivity.kt
+++ app/src/main/java/com/example/apicalls/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var color= arrayOf("#F3FFE3","#C4FAF8","#FFFFD1","#DCD3FF","#FFCBC1","#AFF8DB")
-
+        var jsonData=URL("https://reqres.in/api/users?page=2").readText()
+        println(jsonData)
         val btn=findViewById<Button>(R.id.buttonString)
         btn.setOnClickListener {
             val number=(0..5).random()
